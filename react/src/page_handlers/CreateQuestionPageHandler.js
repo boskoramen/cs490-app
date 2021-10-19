d6a2548d0421d5eeecd4a19b6797f125c13d0fa6
Isaiah <frozendonkeyman@gmail.com>
2021-10-18 23:08:24 -0400

    sets up skeleton for creating questions

---
+++ react/src/page_handlers/CreateQuestionPageHandler.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import CreateQuestionPage from "../pages/CreateQuestionPage";
+import MasterContext from "../reducer/context";
+
+const CreateQuestionPageHandler = (props) => {
+    const { state } = useContext(MasterContext);
+    return (
+        !state.isLoggedIn ?
+        <Redirect to="/" />
+        : <CreateQuestionPage />
+    );
+}
+
+export default CreateQuestionPageHandler;
